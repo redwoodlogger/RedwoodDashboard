bea114c0c71e5b272144521a4519fc548397baa5
mervyn <mail.wyrm@gmail.com>
2018-11-27 22:31:40 +0800

    Set up project with webpack, babel, eslint and prettier

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => <div>Hello there General Kenobi!</div>;
+
+ReactDOM.render(<Index />, document.getElementById("index"));
