1fa63229b10ba965219ca51d65e1fab1077a36e4
mervyn <mail.wyrm@gmail.com>
2019-02-09 11:38:49 +0800

    Create Comment component

--- src/Home/SidePanel.js
+++ src/Home/SidePanel.js
@@ -1,31 +1,96 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import PropTypes from "prop-types";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
+import PropTypes from "prop-types";
 import Button from "../shared/Button";
+import Hashtag from "../shared/Hashtag";
+import UserBubble from "../shared/UserBubble";
 
-const Hashtag = props => {
+const CommentUser = props => {
   const { children } = props;
   return (
-    <button
+    <div
       css={css`
-        background: #bf6ebf;
-        padding: 0.2em 0.5em 0.2em 0.5em;
-        margin: 0 0.2em 0.4em 0;
-        border: none;
-        border-radius: 0.3em;
-        color: white;
+        display: flex;
+        align-items: center;
+        margin-bottom: 0.5em;
       `}
-      type="button"
     >
       {children}
-    </button>
+    </div>
   );
 };
 
-Hashtag.propTypes = {
+const CommentUsername = props => {
+  const { children } = props;
+  return <p>{children}</p>;
+};
+
+CommentUsername.propTypes = {
   children: PropTypes.string.isRequired
+};
+
+const CommentContent = props => {
+  const { children } = props;
+  return <p>{children}</p>;
+};
+
+CommentContent.propTypes = {
+  children: PropTypes.string.isRequired
+};
+
+const CommentDate = props => {
+  const { children } = props;
+  return (
+    <p
+      css={css`
+        font-size: 0.8em;
+        color: #888888;
+        margin-top: 0.5em;
+      `}
+    >
+      {children}
+    </p>
+  );
+};
+
+CommentDate.propTypes = {
+  children: PropTypes.string.isRequired
+};
+
+const Comment = props => {
+  const { user, comment, date } = props;
+  return (
+    <div
+      css={css`
+        background: white;
+        border: 1px solid #e5e5e5;
+        border-radius: 0.5em;
+        padding: 1em;
+        margin-bottom: 0.5em;
+      `}
+    >
+      <CommentUser>
+        <UserBubble
+          css={css`
+            margin-right: 0.5em;
+          `}
+        >
+          {user}
+        </UserBubble>
+        <CommentUsername>{user}</CommentUsername>
+      </CommentUser>
+      <CommentContent>{comment}</CommentContent>
+      <CommentDate>{date}</CommentDate>
+    </div>
+  );
+};
+
+Comment.propTypes = {
+  user: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired
 };
 
 const SidePanel = () => (
@@ -54,16 +119,36 @@ const SidePanel = () => (
       </div>
       <input
         css={css`
-          border: 1px solid #e5e5e5;
+          border: 1px solid #cccccc;
           border-radius: 0.2em;
           width: 100%;
-          height: 2em;
+          height: 2.2em;
+          margin-top: 0.5em;
         `}
-        placeholder="Add a hashtag"
+        placeholder="Add a tag"
       />
-      <Button color="#FF6F61">Submit</Button>
+      <div
+        css={css`
+          display: flex;
+          justify-content: end;
+        `}
+      >
+        <Button
+          css={css`
+            margin-top: 0.3em;
+            margin-left: auto;
+          `}
+          color="#FF6F61"
+        >
+          Add tag
+        </Button>
+      </div>
     </section>
     <p>Dev Remarks</p>
+    <Comment user="Mervyn" comment="Hello there" date="17 February 2019" />
+    <Comment user="Mervyn" comment="Hello there" date="17 February 2019" />
+    <Comment user="Mervyn" comment="Hello there" date="17 February 2019" />
+    <Comment user="Mervyn" comment="Hello there" date="17 February 2019" />
   </aside>
 );
 
