b7767d0872c638e66507460f37d3f9e57d59fa74
ruffy0002 <qinying1313@yahoo.com>
2019-02-11 14:23:54 +0800

    More jsx updates

--- src/Playback/Details.jsx
+++ src/Playback/Details.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Group, GroupTitle } from "./shared/LeftPanelGroup";
 
 const Details = () => (
-  <Group>
-    <GroupTitle>Details</GroupTitle>
-    <p>Ash Ketchum</p>
-    <p>Bug Report #123456</p>
-    <p>System One</p>
-    <p>02 December 2018 3:21pm (GMT +8)</p>
-  </Group>
+	<Group>
+		<GroupTitle>Details</GroupTitle>
+		<p>Ash Ketchum</p>
+		<p>Bug Report #123456</p>
+		<p>System One</p>
+		<p>02 December 2018 3:21pm (GMT +8)</p>
+	</Group>
 );
 
 export default Details;
