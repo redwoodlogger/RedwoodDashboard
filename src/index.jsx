ad177d654cad336a9380f2a0f2d55f7905a5b9da
mervynwee <mail.wyrm@gmail.com>
2019-02-11 15:05:51 +0800

    Fix formatting

--- src/index.jsx
+++ src/index.jsx
@@ -12,171 +12,171 @@ import Playback from "./Playback/Playback";
 library.add(faAngleDown);
 
 const App = () => (
-    <React.Fragment>
-        <Global
-            styles={css`
-                /* http://meyerweb.com/eric/tools/css/reset/ 
+  <React.Fragment>
+    <Global
+      styles={css`
+        /* http://meyerweb.com/eric/tools/css/reset/ 
             v2.0 | 20110126
             License: none (public domain)
         */
 
-                html,
-                body,
-                div,
-                span,
-                applet,
-                object,
-                iframe,
-                h1,
-                h2,
-                h3,
-                h4,
-                h5,
-                h6,
-                p,
-                blockquote,
-                pre,
-                a,
-                abbr,
-                acronym,
-                address,
-                big,
-                cite,
-                code,
-                del,
-                dfn,
-                em,
-                img,
-                ins,
-                kbd,
-                q,
-                s,
-                samp,
-                small,
-                strike,
-                strong,
-                sub,
-                sup,
-                tt,
-                var,
-                b,
-                u,
-                i,
-                center,
-                dl,
-                dt,
-                dd,
-                ol,
-                ul,
-                li,
-                fieldset,
-                form,
-                label,
-                legend,
-                table,
-                caption,
-                tbody,
-                tfoot,
-                thead,
-                tr,
-                th,
-                td,
-                article,
-                aside,
-                canvas,
-                details,
-                embed,
-                figure,
-                figcaption,
-                footer,
-                header,
-                hgroup,
-                menu,
-                nav,
-                output,
-                ruby,
-                section,
-                summary,
-                time,
-                mark,
-                audio,
-                video {
-                    margin: 0;
-                    padding: 0;
-                    border: 0;
-                    font-size: 100%;
-                    font: inherit;
-                    vertical-align: baseline;
-                }
+        html,
+        body,
+        div,
+        span,
+        applet,
+        object,
+        iframe,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        blockquote,
+        pre,
+        a,
+        abbr,
+        acronym,
+        address,
+        big,
+        cite,
+        code,
+        del,
+        dfn,
+        em,
+        img,
+        ins,
+        kbd,
+        q,
+        s,
+        samp,
+        small,
+        strike,
+        strong,
+        sub,
+        sup,
+        tt,
+        var,
+        b,
+        u,
+        i,
+        center,
+        dl,
+        dt,
+        dd,
+        ol,
+        ul,
+        li,
+        fieldset,
+        form,
+        label,
+        legend,
+        table,
+        caption,
+        tbody,
+        tfoot,
+        thead,
+        tr,
+        th,
+        td,
+        article,
+        aside,
+        canvas,
+        details,
+        embed,
+        figure,
+        figcaption,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        output,
+        ruby,
+        section,
+        summary,
+        time,
+        mark,
+        audio,
+        video {
+          margin: 0;
+          padding: 0;
+          border: 0;
+          font-size: 100%;
+          font: inherit;
+          vertical-align: baseline;
+        }
 
-                /* HTML5 display-role reset for older browsers */
-                article,
-                aside,
-                details,
-                figcaption,
-                figure,
-                footer,
-                header,
-                hgroup,
-                menu,
-                nav,
-                section {
-                    display: block;
-                }
-                body {
-                    line-height: 1;
-                }
-                ol,
-                ul {
-                    list-style: none;
-                }
-                blockquote,
-                q {
-                    quotes: none;
-                }
-                blockquote:before,
-                blockquote:after,
-                q:before,
-                q:after {
-                    content: "";
-                    content: none;
-                }
-                table {
-                    border-collapse: collapse;
-                    border-spacing: 0;
-                }
-                /* End of CSS reset */
+        /* HTML5 display-role reset for older browsers */
+        article,
+        aside,
+        details,
+        figcaption,
+        figure,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        section {
+          display: block;
+        }
+        body {
+          line-height: 1;
+        }
+        ol,
+        ul {
+          list-style: none;
+        }
+        blockquote,
+        q {
+          quotes: none;
+        }
+        blockquote:before,
+        blockquote:after,
+        q:before,
+        q:after {
+          content: "";
+          content: none;
+        }
+        table {
+          border-collapse: collapse;
+          border-spacing: 0;
+        }
+        /* End of CSS reset */
 
-                html,
-                body,
-                #index {
-                    margin: 0;
-                    height: 100%;
-                }
+        html,
+        body,
+        #index {
+          margin: 0;
+          height: 100%;
+        }
 
-                /* Set all HTML elements to have border-box */
-                html {
-                    box-sizing: border-box;
-                }
+        /* Set all HTML elements to have border-box */
+        html {
+          box-sizing: border-box;
+        }
 
-                *,
-                *:before,
-                *:after {
-                    box-sizing: inherit;
-                }
-                /* End of border-box reset */
-            `}
-        />
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/playback" component={Playback} />
-        </Switch>
-    </React.Fragment>
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+        /* End of border-box reset */
+      `}
+    />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/playback" component={Playback} />
+    </Switch>
+  </React.Fragment>
 );
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById("index")
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("index")
 );
