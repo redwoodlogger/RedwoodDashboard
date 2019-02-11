f8ce9922809a16e167b7065420d5a85b1577fb00
mervynwee <mail.wyrm@gmail.com>
2019-02-11 11:15:11 +0800

    Move SystemDropdown component into its own file

--- src/Home/LogsPanel.js
+++ src/Home/LogsPanel.js
@@ -3,76 +3,11 @@ import React, { Component } from "react";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import PropTypes from "prop-types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { AgGridReact } from "ag-grid-react";
+import SystemDropdown from "./SystemDropdown";
 import Button from "../shared/Button";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
-
-class SystemDropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false
-    };
-  }
-
-  onClick = () => {
-    const { open } = this.state;
-    this.setState({ open: !open });
-  };
-
-  render() {
-    const { systems, currentSystem } = this.props;
-    const { open } = this.state;
-    return (
-      <div
-        css={css`
-          position: relative;
-          display: flex;
-          align-items: center;
-          margin: 0 0 0.5em 0;
-        `}
-      >
-        <p
-          onClick={this.onClick}
-          css={css`
-            font-size: 1.3rem;
-            margin-right: 0.3em;
-            cursor: pointer;
-          `}
-        >
-          {currentSystem}
-        </p>
-        <FontAwesomeIcon
-          onClick={this.onClick}
-          css={css`
-            cursor: pointer;
-          `}
-          icon="angle-down"
-        />
-        <div
-          css={css`
-            display: ${open ? "block" : "none"};
-            position: absolute;
-            top: 1.5em;
-            height: 10em;
-            width: 10em;
-            background: white;
-            border: 1px solid #e5e5e5;
-            border-radius: 0.5em;
-            z-index: 1;
-          `}
-        />
-      </div>
-    );
-  }
-}
-
-SystemDropdown.propTypes = {
-  systems: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  currentSystem: PropTypes.string.isRequired
-};
 
 const PanelButtons = () => (
   <div>
---
+++ src/Home/SystemDropdown.js
@@ -0,0 +1,73 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from "react";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class SystemDropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  onClick = () => {
+    const { open } = this.state;
+    this.setState({ open: !open });
+  };
+
+  render() {
+    const { systems, currentSystem } = this.props;
+    const { open } = this.state;
+    return (
+      <div
+        css={css`
+          position: relative;
+          display: flex;
+          align-items: center;
+          margin: 0 0 0.5em 0;
+        `}
+      >
+        <p
+          onClick={this.onClick}
+          css={css`
+            font-size: 1.3rem;
+            margin-right: 0.3em;
+            cursor: pointer;
+          `}
+        >
+          {currentSystem}
+        </p>
+        <FontAwesomeIcon
+          onClick={this.onClick}
+          css={css`
+            cursor: pointer;
+          `}
+          icon="angle-down"
+        />
+        <div
+          css={css`
+            display: ${open ? "block" : "none"};
+            position: absolute;
+            top: 1.5em;
+            height: 10em;
+            width: 10em;
+            background: white;
+            border: 1px solid #e5e5e5;
+            border-radius: 0.5em;
+            z-index: 1;
+          `}
+        />
+      </div>
+    );
+  }
+}
+
+SystemDropdown.propTypes = {
+  systems: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  currentSystem: PropTypes.string.isRequired
+};
+
+export default SystemDropdown;
