a22e805e431a9e2e2ca5975d169ee95aee76913f
leeyingjie <leeyingjie_95@hotmail.com>
2019-02-14 09:09:35 +0800

    Added colour file

---
+++ src/Home/LogsPanel/ColourData.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+export const ColourData = [
+  "#70C14A",
+  "#33CC94",
+  "#31C3D9",
+  "#4983C4",
+  "#A25AA1",
+  "#DD4C79",
+  "#EE5545",
+  "#F28728",
+  "#E6B211",
+  "#AE8C4C"
+];
